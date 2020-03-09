9ff1327f0de69deaf9bfbfb70dfb62c52baed010
ktn1337wickedsick <ktn1337wickedsick@gmail.com>
2020-03-09 22:20:17 +0100

    fixing server again

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static('public'))
 
-const port = proces.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server is up and runing on port ${port}`)
